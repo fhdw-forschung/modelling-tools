7e529c17b913eb16e648b3dda3f20d3f028c60f2
Pascal Niggemeier <pascal.niggemeier@fhdw.de>
2024-01-22 19:28:07 +0000

    (#92) add prefix for logged metrics

--- fhdw/modelling/tracking.py
+++ fhdw/modelling/tracking.py
@@ -5,7 +5,11 @@ import mlflow
 from fhdw.modelling.evaluation import get_regression_metrics
 
 
-def log_metrics_to_mlflow(y_true, y_pred):
+def log_metrics_to_mlflow(y_true, y_pred, prefix: str = ""):
     """Log metrics to active MLflow Experiment and Run."""
-    mlflow.log_metrics(metrics=get_regression_metrics(y_true=y_true, y_pred=y_pred))
+    prefix = f"{prefix}_" if prefix and prefix[-1] != "_" else prefix
+
+    metrics = get_regression_metrics(y_true=y_true, y_pred=y_pred)
+    metrics = {f"{prefix}{metric}": v for metric, v in metrics.items()}
+    mlflow.log_metrics(metrics=metrics)
     return True
